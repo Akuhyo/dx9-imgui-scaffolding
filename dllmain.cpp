f3932d304135ce9335736c9aaf1850ae3d0a7167
Akuhyo <dgL7@protonmail.com>
2020-04-20 14:27:02 -0500

    Add files via upload

---
+++ dllmain.cpp
@@ -0,0 +1,145 @@
+#include "utils.h"
+
+#include "source-sdk/sdk.hpp"
+
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+
+void* d3d9Device[119];
+BYTE EndSceneBytes[7]{ 0 };
+tEndScene oEndScene = nullptr;
+
+extern LPDIRECT3DDEVICE9 pDevice = nullptr;
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+//i_material ggg;
+
+WNDPROC oWndProc;
+DWORD* D3d9VTable;
+DWORD D3d9Base;
+
+bool showMenu = true;
+bool chamsOn = false;
+
+const char* windowName = "Counter-Strike: Global Offensive";
+
+void APIENTRY hkEndScene(LPDIRECT3DDEVICE9 o_pDevice) {
+    if (!pDevice)
+        pDevice = o_pDevice;
+    
+
+    /*
+    if (!initialized) InitImGui(pDevice);
+    else {
+        ImGui_ImplDX9_NewFrame();
+        ImGui_ImplWin32_NewFrame();
+
+        ImGui::NewFrame();
+
+        if (g_ShowMenu)
+        {
+            bool bShow = true;
+            ImGui::ShowDemoWindow(&bShow);
+        }
+
+        ImGui::EndFrame();
+        ImGui::Render();
+        ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+    }
+    */
+    
+    static bool init = true;
+    if (init)
+    {
+        init = false;
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO();
+
+        ImGui_ImplWin32_Init(FindWindowA(NULL, windowName));
+        ImGui_ImplDX9_Init(pDevice);
+    }
+
+    if (showMenu) {
+        ImGui_ImplDX9_NewFrame();
+        ImGui_ImplWin32_NewFrame();
+
+
+        ImGui::NewFrame();
+
+        ImVec2 windSize;
+        windSize.x = 300;
+        windSize.y = 300;
+
+
+
+        ImGui::Begin("Window");
+
+        ImGui::Text("Hello, world!");
+        ImGui::Checkbox("Chams", &chamsOn);
+
+        ImGui::SetWindowSize("Window", windSize);
+
+        //ImGui::EndFrame();
+
+        ImGui::Render();
+
+        ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+    }
+
+    oEndScene(pDevice);
+}
+
+LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    /*
+    if (true) {
+        return ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam);
+    }
+    */
+
+    if(showMenu && ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
+        return true;
+
+    return CallWindowProc(oWndProc, hWnd, uMsg, wParam, lParam);
+}
+
+DWORD WINAPI mainThread(HMODULE hModule) {
+    if (GetD3D9Device(d3d9Device, sizeof(d3d9Device))) {
+        memcpy(EndSceneBytes, (char*)d3d9Device[42], 7);
+
+        oEndScene = (tEndScene)TrampHook((char*)d3d9Device[42], (char*)hkEndScene, 7);
+        oWndProc = (WNDPROC)SetWindowLongPtr(window, GWL_WNDPROC, (LONG_PTR)WndProc);
+    }
+
+    while (!GetAsyncKeyState(VK_END)) {
+
+        if (GetAsyncKeyState(VK_INSERT)) {
+            showMenu = !showMenu;
+            Sleep(500);
+        }
+
+    }
+
+    Patch((BYTE*)d3d9Device[42], EndSceneBytes, 7);
+
+    Sleep(1000);
+
+    FreeLibraryAndExitThread(hModule, 0);
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)mainThread, hModule, 0, nullptr));
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ utils.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <Windows.h>
+
+#include "ImGui/imgui.h";
+#include "ImGui/imconfig.h";
+#include "ImGui/imgui_internal.h";
+#include "ImGui/imstb_rectpack.h";
+#include "ImGui/imstb_textedit.h";
+#include "ImGui/imstb_truetype.h";
+#include "ImGui/imgui_impl_dx9.h";
+#include "ImGui/imgui_impl_win32.h";
+
+#include "source-sdk/structs/materials.hpp"
+
+#include <d3d9.h>
+#include <d3dx9.h>
+
+#include "directx.h"
+#include "hooks.h"
